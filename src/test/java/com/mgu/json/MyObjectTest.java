a6a999d88918ba30e2b84f2ac9be035c3775594d
Marc Guerrini <maguerrini@airfrance.fr>
2021-08-25 13:27:08 +0200

    test modified

--- src/test/java/com/mgu/json/MyObjectTest.java
+++ src/test/java/com/mgu/json/MyObjectTest.java
@@ -5,52 +5,30 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyObjectTest {
-
-    @Test
-    @DisplayName("test the unmarshalling with one profile as a list")
-    void testTheUnmarshallingWithOneProfileAsAList() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        String userInfoAsJson = "{\n"
-                + "  \"profile\" : [\"P1\"]\n"
-                + "}";
-
-
-        TypeReference<MyObject> typeRef = new TypeReference<MyObject>() {};
-        MyObject ui = mapper.readValue(userInfoAsJson, typeRef);
-        assertEquals(ui.getProfile().size(),1);
-    }
-
     @Test
     @DisplayName("test the unmarshalling with multiple profiles")
     void testTheUnmarshallingWithMultipleProfiles() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        String userInfoAsJson = "{\n"
-                + "  \"profile\" : [\"P1\", \"P2\"]\n"
-                + "}";
+        String userInfoAsJson = "{\"profile\" : [\"P1\", \"P2\"]}";
 
         TypeReference<MyObject> typeRef = new TypeReference<MyObject>() {};
         MyObject ui = mapper.readValue(userInfoAsJson, typeRef);
         assertEquals(ui.getProfile().size(),2);
     }
 
-    @Test
-    @DisplayName("test the unmarshalling with one profile as a single string")
-    void testTheUnmarshallingWithOneProfileAsASingleString() throws JsonProcessingException {
+    @DisplayName("test the unmarshalling with one profile")
+    @ParameterizedTest
+    @ValueSource(strings = {"{\"profile\" : \"P1\"}", "{\"profile\" : [\"P1\"]}"})
+    void testTheUnmarshallingWithOneProfile(String userInfoAsJson) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        String userInfoAsJson = "{\n"
-                + "  \"profile\" : \"P1\"\n"
-                + "}";
-
-
         TypeReference<MyObject> typeRef = new TypeReference<MyObject>() {};
         MyObject ui = mapper.readValue(userInfoAsJson, typeRef);
         assertEquals(ui.getProfile().size(),1);
     }
-
 }
